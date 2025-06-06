e0eb58fed82fba0d2d7d582ca80295a2f4d2273f
Christian Gastrell <cgastrell@gmail.com>
2025-06-06 14:43:42 -0300

    remove PREFERRED_VIEW, preferredView and fix URLs

--- projects/packages/forms/src/blocks/contact-form/class-contact-form-block.php
+++ projects/packages/forms/src/blocks/contact-form/class-contact-form-block.php
@@ -778,12 +778,9 @@ class Contact_Form_Block {
 			)
 		);
 
-		// Create a Contact_Form instance to get the default values
-		$dashboard_view_switch   = new Dashboard_View_Switch();
-		$form_responses_url      = $dashboard_view_switch->get_forms_admin_url();
+		$form_responses_url      = Dashboard_View_Switch::get_forms_admin_url();
 		$akismet_active_with_key = Jetpack::is_akismet_active();
 		$akismet_key_url         = admin_url( 'admin.php?page=akismet-key-config' );
-		$preferred_view          = $dashboard_view_switch->get_preferred_view();
 
 		$data = array(
 			'defaults' => array(
@@ -793,8 +790,6 @@ class Contact_Form_Block {
 				'akismetActiveWithKey' => $akismet_active_with_key,
 				'akismetUrl'           => $akismet_key_url,
 				'assetsUrl'            => Jetpack_Forms::assets_url(),
-				'preferredView'        => $preferred_view,
-				'isMailPoetEnabled'    => Jetpack_Forms::is_mailpoet_enabled(),
 			),
 		);
 
