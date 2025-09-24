7d3c417487370b5d7f92ee8adbbf424f67a77f5b
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-09-24 14:25:05 -0700

    Revert "We want to keep the endpoints the same so that we get the benefit of the preloading"
    This reverts commit ed0d56f1c14b0240eaee04dc27c489fe36d89774.

--- projects/packages/forms/src/dashboard/class-dashboard.php
+++ projects/packages/forms/src/dashboard/class-dashboard.php
@@ -84,8 +84,8 @@ class Dashboard {
 
 		// Preload Forms endpoints needed in dashboard context.
 		$preload_paths = array(
-			'/wp/v2/feedback/config',
-			'/wp/v2/feedback/integrations?version=2',
+			'/wp/v2/feedback/config?_fields=sync',
+			'/wp/v2/feedback/integrations?version=2&_fields=sync',
 		);
 		$preload_data  = array_reduce( $preload_paths, 'rest_preload_api_request', array() );
 		wp_add_inline_script(
